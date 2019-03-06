d51c424827cf76836803f51b34c6296344cea5bd
Saksham Kashyap <sakshamshady@gmail.com>
2019-03-06 23:08:53 +0530

    twitter

---
+++ BlogSite/auth.py
@@ -0,0 +1,4 @@
+consumer_key        = 'cJdrqjt0HfKaI25ZZbJoPSIDx'
+consumer_secret     = '4h2ARS9OpBDTXA6bh2sX0H0ERMGL1nhzThm83qhh7G4ZsVNWTB'
+access_token        = '941558295621287936-vgiwwB8ZElaaSVfUbG3gf4pC3PRAqRe'
+access_token_secret = 'Z7uEI9xUjMzEeTBmcu1TqaDTZGKovxjQ4NdbqF74kH7kS'
